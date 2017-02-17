cdc5ad1a4599dd680674aa0cd7059496b236159b
Alicja Ćmiel <cmielalicja@gmail.com>
2017-02-17 11:18:41 +0000

    select menu

--- app/app.js
+++ app/app.js
@@ -1,152 +1,174 @@
-document.addEventListener("DOMContentLoaded", function(){    
+document.addEventListener("DOMContentLoaded", function() {
     function dropDownMenu() {
-        
-      var menu = document.querySelectorAll(".center> ul >li");
-    
-   for (var i = 0; i < menu.length; i++) {
-        
-        menu[i].addEventListener("mouseover", function(event){
-           
-           var dropDown = this.querySelector("ul");
-            
-            if (dropDown != null) {
-                
-                 dropDown.style.display= "block";
+
+        var menu = document.querySelectorAll(".center> ul >li");
+
+        for (var i = 0; i < menu.length; i++) {
+
+            menu[i].addEventListener("mouseover", function(event) {
+
+                var dropDown = this.querySelector("ul");
+
+                if (dropDown != null) {
+
+                    dropDown.style.display = "block";
                 }
-                
+
             });
 
-        
-        menu[i].addEventListener("mouseout", function(event){
-            
-            var dropDown = this.querySelector("ul");
-            
-            if (dropDown != null) {
-                
-                 dropDown.style.display= "none";
+
+            menu[i].addEventListener("mouseout", function(event) {
+
+                var dropDown = this.querySelector("ul");
+
+                if (dropDown != null) {
+
+                    dropDown.style.display = "none";
                 }
-                
-            });        
-         }
+
+            });
+        }
     }
     dropDownMenu();
-    
+
     function hideChairName() {
-        
+
         var img1 = document.querySelector(".img1");
-        
-        
-        img1.addEventListener("mouseover", function mouseOver(event){
+
+
+        img1.addEventListener("mouseover", function mouseOver(event) {
             img1.classList.remove("chair-name");
-            
+
         });
-        
-        img1.addEventListener("mouseleave", function mouseOver(event){
+
+        img1.addEventListener("mouseleave", function mouseOver(event) {
             img1.classList.add("chair-name");
-            
+
         });
-        
+
         var img2 = document.querySelector(".img2");
-    
-        
-        img2.addEventListener("mouseover", function mouseOver(event){
+
+
+        img2.addEventListener("mouseover", function mouseOver(event) {
             img2.classList.remove("chair-name-2");
-            
+
         });
-        
-        img2.addEventListener("mouseleave", function mouseOver(event){
+
+        img2.addEventListener("mouseleave", function mouseOver(event) {
             img2.classList.add("chair-name-2");
-            
+
         });
     }
     hideChairName();
-    
+
     function slider() {
-        
-        
-  var next = document.querySelector(".right");
-  var prev = document.querySelector(".left");
-  var images = document.querySelectorAll(".slider li");
-  var imageCount = 0;
-    
-    //console.log(next);
-    //console.log(prev);
-    //console.log(images);
-    //console.log(imageCount);
-    
-    images[imageCount].classList.add('visible');
-    
 
-    next.addEventListener("click", function(event){
-        
-        //console.log("click");
-        
-        images[imageCount].classList.remove('visible');
-        
-        imageCount ++;
-        
-        if (imageCount >= images.length) {
-            imageCount = 0;
-        }
-        
+
+        var next = document.querySelector(".right");
+        var prev = document.querySelector(".left");
+        var images = document.querySelectorAll(".slider li");
+        var imageCount = 0;
+
+        //console.log(next);
+        //console.log(prev);
+        //console.log(images);
+        //console.log(imageCount);
+
         images[imageCount].classList.add('visible');
-        
+
+
+        next.addEventListener("click", function(event) {
+
+            //console.log("click");
+
+            images[imageCount].classList.remove('visible');
+
+            imageCount++;
+
+            if (imageCount >= images.length) {
+                imageCount = 0;
+            }
+
+            images[imageCount].classList.add('visible');
+
         });
-    
-    
 
-    prev.addEventListener("click", function(event){
-        
-        //console.log("click");
-        
-        images[imageCount].classList.remove('visible');
-        
-        imageCount --;
-        
-        if (imageCount < 0) {
-            imageCount = images.length - 1;
-        }
-        
-        images[imageCount].classList.add('visible');
-        
-        
+
+
+        prev.addEventListener("click", function(event) {
+
+            //console.log("click");
+
+            images[imageCount].classList.remove('visible');
+
+            imageCount--;
+
+            if (imageCount < 0) {
+                imageCount = images.length - 1;
+            }
+
+            images[imageCount].classList.add('visible');
+
+
         });
     }
-    
+
     slider();
-    
-    function selectPlan() {
-        
-        
-        
-        
-        
-    }
-    selectPlan();
-    
+
     function select() {
-     
+
         var subMenu = document.querySelectorAll(".list_arrow");
         console.log(subMenu);
-        
+
+        var list_panel = document.querySelectorAll(".list_panel");
+
+        var options = document.querySelectorAll(".list_panel li");
+        console.log(options);
+
+        var selectedChair = document.querySelector(".title-sum");
+        console.log(selectedChair);
+
+        var chairValue = document.querySelector(".panel_right h4");
+        console.log(chairValue);
+
+        // var selectedColor = document.querySelector(".color");
+        // console.log(selectedColor);
+        //
+        // var chairValue = document.querySelector(".panel_right .color");
+        // console.log(chairValue);
+
         for (var i = 0; i < subMenu.length; i++) {
-            
+
             subMenu[i].addEventListener("click", function onclick (event){
-                
+
                 this.nextElementSibling.style.display="block";
-                
+
             });
-            
-            
-        }
-        
-        
-        
-        
-        
-        
-        
+
+
+              for (var j = 0; j < options.length; j ++) {
+
+                  options[j].addEventListener("click", function onclick (event){
+                    this.parentNode.style.display="none";
+
+                    selectedChair.innerText = "Chair " + this.innerText;
+
+                    chairValue.innerText = this.dataset.price;
+
+                    // selectedColor.innerText = "Color " + this.innerText;
+
+
+                  });
+
+              }
+            }
+
+
+
+
+
+
     }
     select();
-    
+
 });
